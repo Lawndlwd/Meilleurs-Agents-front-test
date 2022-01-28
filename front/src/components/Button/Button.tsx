eab231771b7a931ef809e87de0a9b3c3671d3301
Levende <lwdlwd95@gmail.com>
2022-01-28 17:20:32 +0100

    color change when unread msg is 0

--- front/src/components/Button/Button.tsx
+++ front/src/components/Button/Button.tsx
@@ -4,7 +4,9 @@ import message from 'assets/message.svg';
 import React from 'react';
 export const Button = ({ messageunreded }: { messageunreded: number }) => {
   return (
-    <div className="Button">
+    <div
+      className="Button"
+      style={{ backgroundColor: messageunreded === 0 ? '#777' : '' }}>
       <img className="Button_logo" src={message} alt="message" />
       <span>{messageunreded}</span>
     </div>
